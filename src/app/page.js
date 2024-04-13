c372b5fcb3927a2d90a26ee9d876bb205a536631
jasa1aw <erakrut05@gmail.com>
2024-04-13 15:07:00 +0500

    homepage layout is done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 'use client'
-import UserLogin from "@/components/auth/userLogin";
+// import UserLogin from "@/components/auth/userLogin";
+import Main from "@/components/main";
 export default function Home() {
   return (
     <main>
-      <UserLogin/>
+      <Main/>
+      {/* <UserLogin/> */}
     </main>
   );
 }
---
+++ src/components/main/index.jsx
@@ -0,0 +1,12 @@
+export default function Main() {
+    return(
+        <section className="main">
+            <h1>Добро пожаловать!</h1>
+            <h4>Lorby - твой личный репетитор</h4>
+            <div className="logo">
+                <img src="/img/Illustration.svg" alt="Not Found" className="img" />
+            </div>
+            <button className="button">Выйти</button>
+        </section>
+    )
+}
